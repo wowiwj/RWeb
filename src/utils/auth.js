4e92ce40c4dd21154e85e11f1c70eb12e855bf46
王举 <35649084@qq.com>
2018-05-25 18:19:49 +0800

    add login register page

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,3 +3,9 @@ const ApiKey = 'api-key'
 export function getToken () {
   return localStorage.getItem(ApiKey)
 }
+export function setToken (token) {
+  return localStorage.setItem(ApiKey,token)
+}
+export function removeToken () {
+  return localStorage.removeItem(ApiKey)
+}
