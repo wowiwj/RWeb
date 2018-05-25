880520aa016382b1bf53cbb60947e44fe4923a74
王举 <35649084@qq.com>
2018-05-25 17:49:02 +0800

    取消了坑爹的提示报错问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,11 @@ module.exports = {
   },
   env: {
     browser: true,
+    node: true,
+    es6: true,
   },
   extends: [
+    "eslint:recommended",
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential', 
